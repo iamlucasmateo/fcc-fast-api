6e5a349db4ff9f58ccd3157923da8dfa34cc6ffc
iamlucasmateo <perrone.lucasmateo@gmail.com>
2022-06-10 07:25:22 -0300

    votes endpoints

--- src/routers/likes.py
+++ src/routers/likes.py
@@ -8,8 +8,8 @@ from src.models import UpDownVote
 from src.schemas import UserResponse
 
 router = APIRouter(
-    prefix="/likes",
-    tags=["Posts"]
+    prefix="/votes",
+    tags=["Votes"]
 )
 
 @router.post("/{id}")
@@ -24,7 +24,8 @@ async def like_post(
         db.add(vote) # creates row in DB
         db.commit() # commits
         db.refresh(vote) # retrieves results from DB
-        return {"messsage": "like created"}
+        updown = "up" if upvote else "down"
+        return {"messsage": f"created {updown}vote"}
     except IntegrityError as e:
         print(e)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
@@ -32,7 +33,41 @@ async def like_post(
 
 @router.delete("/{id}")
 async def delete_like(
+    id: int,
     db: Session = Depends(get_db),
-    user: int = Depends(get_current_user)
+    current_user: int = Depends(get_current_user)
 ):
-    pass
+    query = db.query(UpDownVote).filter(
+        UpDownVote.post_id == id, UpDownVote.user_id == current_user.id
+    )
+    if query.first() == None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User has no vote for this post")
+    
+    query.delete(synchronize_session=False)
+    db.commit()
+    return {"message": f"Post {id} successfully deleted"}
+
+
+@router.get("/")
+async def get_all_user_votes(
+    current_user: int = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    # paginate this
+    query = db.query(UpDownVote).filter(UpDownVote.user_id == current_user.id)
+    return query.all()
+
+
+@router.get("/{post_id}")
+async def get_all_user_votes(
+    post_id: int,
+    db: Session = Depends(get_db)
+):  
+    # alternative: use COUNT
+    query = db.query(UpDownVote).filter(UpDownVote.post_id == post_id)
+    results = query.all()
+    upvotes = len([r for r in results if r.upvote])
+    downvotes = len([r for r in results if not r.upvote])
+    return {"upvotes": upvotes, "downvotes": downvotes}
+
+
