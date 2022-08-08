f0aed053eda9b94d165686b0e1e038167dae7e9e
iamlucasmateo <perrone.lucasmateo@gmail.com>
2022-08-08 09:52:38 -0300

    cors middleware + requests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.models import Base
 from src.database import SessionLocal, engine
@@ -8,7 +9,7 @@ from src.routers import posts, user, repository, auth, likes
 # this will create the tables is they not exist, otherwise it will use them
 # Base.metadata.create_all(bind=engine)
 
-# with Alembic it's not neede anymore
+# with Alembic it's not needed anymore
 
 app = FastAPI()
 
@@ -17,3 +18,25 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(likes.router)
 app.include_router(repository.router)
+
+@app.get("/status")
+def get_status():
+    return {"message": "server running"}
+
+@app.get("/")
+def hello_world():
+    return {"hello": "world"}
+
+# explicit origins declarations
+origins = [
+    "http://localhost",
+    "https://www.google.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
